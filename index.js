673d19173828b0b5ecbc494056ad08f19a4388c8
Seth White <seth.white@strakertranslations.com>
2022-09-01 15:51:09 -0400

    got fake jwts working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const encode = require("jwt-encode");
 const crypto = require("crypto");
 const app = express();
 app.use(cookieParser());
-
+// app.use(cors());
 let jwt = "";
 let userID = crypto.randomUUID();
 const refreshJWT = () => {
@@ -19,9 +19,21 @@ const refreshJWT = () => {
 	);
 };
 refreshJWT();
-setInterval(refreshJWT, 30000);
-
+setInterval(refreshJWT, 10000);
+app.use((req, res, next) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		req.headers?.referer?.slice(0, -1) || "http://localhost:3000"
+	);
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	next();
+});
 app.get("/jwt", (req, res) => {
+	console.log(req.cookies);
 	if (!req.cookies["fake-session"]) {
 		return res.sendStatus(401);
 	}
@@ -42,7 +54,6 @@ app.post("/login", (req, res) => {
 	let date = new Date();
 	date.setMinutes(date.getMinutes() + 5);
 	let expires = Date.now() + 1000 * 60 * 5;
-	console.log(typeof expires);
 	res.cookie("fake-session", "fake-value", {
 		expires: new Date(expires),
 	});
@@ -53,7 +64,7 @@ app.get("/protected-data", (req, res) => {
 	if (!req.headers.authorization || !req.headers.authorization.includes(jwt)) {
 		return res.status(401).send("Unauthorized");
 	}
-	res.send([{ name: "test project", id: 1 }]);
+	res.send({ name: crypto.randomUUID(), id: 1 });
 });
 
 app.listen(3535);
