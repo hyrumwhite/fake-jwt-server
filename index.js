88af3b8255d038f2480dd17baeb22ac1ecd932da
Seth White <seth.white@strakertranslations.com>
2022-09-01 12:51:57 -0400

    added protected data endpoint

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const encode = require("jwt-encode");
+const crypto = require("crypto");
 const app = express();
 app.use(cookieParser());
 
+let jwt = "";
+let userID = crypto.randomUUID();
+const refreshJWT = () => {
+	console.log("updated jwt");
+	jwt = encode(
+		{
+			name: "John Doesn't",
+			uuid: userID,
+			timestamp: Date.now(),
+		},
+		""
+	);
+};
+refreshJWT();
+setInterval(refreshJWT, 30000);
+
 app.get("/jwt", (req, res) => {
-  if (!req.cookies["fake-session"]) {
-    return res.sendStatus(401);
-  }
-  res.header("content-type", "text/plain");
-  res.send(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-  );
+	if (!req.cookies["fake-session"]) {
+		return res.sendStatus(401);
+	}
+	res.header("content-type", "text/plain");
+	res.send(jwt);
 });
 
 app.get("/login", (req, res) => {
-  res.header("content-type", "text/html");
-  res.send(/*html*/ `
+	res.header("content-type", "text/html");
+	res.send(/*html*/ `
 		<form action="/login" method="POST">
 			<input type="submit" value="login">
 		</form>
@@ -23,14 +39,21 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  let date = new Date();
-  date.setMinutes(date.getMinutes() + 5);
-  let expires = Date.now() + 1000 * 60 * 5;
-  console.log(typeof expires);
-  res.cookie("fake-session", "fake-value", {
-    expires: new Date(expires),
-  });
-  res.redirect("http://localhost:3000");
+	let date = new Date();
+	date.setMinutes(date.getMinutes() + 5);
+	let expires = Date.now() + 1000 * 60 * 5;
+	console.log(typeof expires);
+	res.cookie("fake-session", "fake-value", {
+		expires: new Date(expires),
+	});
+	res.redirect("http://localhost:3000");
+});
+
+app.get("/protected-data", (req, res) => {
+	if (!req.headers.authorization || !req.headers.authorization.includes(jwt)) {
+		return res.status(401).send("Unauthorized");
+	}
+	res.send([{ name: "test project", id: 1 }]);
 });
 
 app.listen(3535);
