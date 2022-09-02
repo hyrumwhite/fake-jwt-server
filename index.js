f8640d84678bfe55dc8b40653d099b159416f6a3
Seth <hyrum.s.white@gmail.com>
2022-09-01 20:35:24 -0400

    added permissions endpoint

--- index.js
+++ index.js
@@ -8,42 +8,42 @@ app.use(cookieParser());
 let jwt = "";
 let userID = crypto.randomUUID();
 const refreshJWT = () => {
-	console.log("updated jwt");
-	jwt = encode(
-		{
-			name: "John Doesn't",
-			uuid: userID,
-			timestamp: Date.now(),
-		},
-		""
-	);
+  console.log("updated jwt");
+  jwt = encode(
+    {
+      name: "John Doesn't",
+      uuid: userID,
+      timestamp: Date.now(),
+    },
+    ""
+  );
 };
 refreshJWT();
 setInterval(refreshJWT, 10000);
 app.use((req, res, next) => {
-	res.header(
-		"Access-Control-Allow-Origin",
-		req.headers?.referer?.slice(0, -1) || "http://localhost:3000"
-	);
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
-	next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    req.headers?.referer?.slice(0, -1) || "http://localhost:3000"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 app.get("/jwt", (req, res) => {
-	console.log(req.cookies);
-	if (!req.cookies["fake-session"]) {
-		return res.sendStatus(401);
-	}
-	res.header("content-type", "text/plain");
-	res.send(jwt);
+  console.log(req.cookies);
+  if (!req.cookies["fake-session"]) {
+    return res.sendStatus(401);
+  }
+  res.header("content-type", "text/plain");
+  res.send(jwt);
 });
 
 app.get("/login", (req, res) => {
-	res.header("content-type", "text/html");
-	res.send(/*html*/ `
+  res.header("content-type", "text/html");
+  res.send(/*html*/ `
 		<form action="/login" method="POST">
 			<input type="submit" value="login">
 		</form>
@@ -51,20 +51,27 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-	let date = new Date();
-	date.setMinutes(date.getMinutes() + 5);
-	let expires = Date.now() + 1000 * 60 * 5;
-	res.cookie("fake-session", "fake-value", {
-		expires: new Date(expires),
-	});
-	res.redirect("http://localhost:3000");
+  let date = new Date();
+  date.setMinutes(date.getMinutes() + 5);
+  let expires = Date.now() + 1000 * 60 * 5;
+  res.cookie("fake-session", "fake-value", {
+    expires: new Date(expires),
+  });
+  res.redirect("http://localhost:3000");
 });
 
 app.get("/protected-data", (req, res) => {
-	if (!req.headers.authorization || !req.headers.authorization.includes(jwt)) {
-		return res.status(401).send("Unauthorized");
-	}
-	res.send({ name: crypto.randomUUID(), id: 1 });
+  if (!req.headers.authorization || !req.headers.authorization.includes(jwt)) {
+    return res.status(401).send("Unauthorized");
+  }
+  res.send({ name: crypto.randomUUID(), id: 1 });
+});
+
+app.get("/permissions", (req, res) => {
+  if (!req.headers.authorization || !req.headers.authorization.includes(jwt)) {
+    return res.status(401).send("Unauthorized");
+  }
+  res.send({ can_get_data: true });
 });
 
 app.listen(3535);
